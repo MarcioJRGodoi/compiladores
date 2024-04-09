9b6df3427f1e59112432999dc8efc820b43aca30
Henry <henrysantoss29@gmail.com>
2024-04-09 20:16:20 -0300

    Henry

--- src/main/java/enuns/CodigoToken.java
+++ src/main/java/enuns/CodigoToken.java
@@ -1,60 +1,76 @@
 package enuns;
 
 public enum CodigoToken {
-    WHILE(1),
-    VOID(2),
-    STRING(3),
-    RETURN(4),
-    NUMERO_INTEIRO(5),
-    NUMERO_FLOAT(6),
-    NOME_VARIAVEL(7),
-    NOME_CHAR(8),
-    NOME_VARIAVEL_2(9),
-    NOME_STRING(10),
-    MAIN(11),
-    LITERAL(12),
-    INTEGER(13),
-    INICIO(14),
-    IF(15),
-    I(16),
-    FOR(17),
-    FLOAT(18),
-    FIM(19),
-    ELSE(20),
-    DO(21),
-    COUT(22),
-    CIN(23),
-    CHAR(24),
-    CALL_FUNCAO(25),
-    MAIOR_IGUAL(26),
-    MAIOR(27),
-    IGUAL(28),
-    ATRIBUICAO(29),
-    MENOR_IGUAL(30),
-    MENOR(31),
-    INCREMENTO(32),
-    ADICAO(33),
-    FECHA_CHAVES(34),
-    ABRE_CHAVES(35),
-    PONTO_VIRGULA(36),
-    DOIS_PONTOS(37),
-    DIVISAO(38),
-    VIRGULA(39),
-    MULTIPLICACAO(40),
-    FECHA_PARENTESES(41),
-    ABRE_PARENTESES(42),
-    DOLLAR(43),
-    DIFERENTE(44),
-    DECREMENTO(45),
-    SUBTRACAO(46);
+    WHILE(1, "while"),
+    VOID(2, "void"),
+    STRING(3, "string"),
+    RETURN(4, "return"),
+    NUMERO_INTEIRO(5, "numerointeiro"),
+    NUMERO_FLOAT(6, "numerofloat"),
+    NOME_VARIAVEL(7, "nomevariavel"),
+    NOME_CHAR(8, "nomedochar"),
+    NOME_VARIAVEL_2(9, "nomedavariavel"),
+    NOME_STRING(10, "nomedastring"),
+    MAIN(11, "main"),
+    LITERAL(12, "literal"),
+    INTEGER(13, "integer"),
+    INICIO(14, "inicio"),
+    IF(15, "if"),
+    I(16, "î"),
+    FOR(17, "for"),
+    FLOAT(18, "float"),
+    FIM(19, "fim"),
+    ELSE(20, "else"),
+    DO(21, "do"),
+    COUT(22, "cout"),
+    CIN(23, "cin"),
+    CHAR(24, "char"),
+    CALL_FUNCAO(25, "callfuncao"),
+    MAIOR_IGUAL(26, ">>"),
+    MAIOR(27, ">"),
+    IGUAL(28, "=="),
+    ATRIBUICAO(29, "="),
+    MENOR_IGUAL(30, "<="),
+    SHIFT_LEFT(31, "<<"),
+    MENOR(32, "<"),
+    INCREMENTO(33, "++"),
+    ADICAO(34, "+"),
+    FECHA_CHAVES(35, "}"),
+    ABRE_CHAVES(36, "{"),
+    PONTO_VIRGULA(37, ";"),
+    DOIS_PONTOS(38, ":"),
+    DIVISAO(39, "/"),
+    VIRGULA(40, ","),
+    MULTIPLICACAO(41, "*"),
+    FECHA_PARENTESES(42, ")"),
+    ABRE_PARENTESES(43, "("),
+    DOLLAR(44, "$"),
+    DIFERENTE(45, "!="),
+    DECREMENTO(46, "--"),
+    SUBTRACAO(47, "-");
 
     private final int codigo;
+    private final String tokenString;
 
-    CodigoToken(int codigo) {
+    CodigoToken(int codigo, String tokenString) {
         this.codigo = codigo;
+        this.tokenString = tokenString;
     }
 
     public int getCodigo() {
-        return this.codigo;
+        return codigo;
+    }
+
+    public String getTokenString() {
+        return tokenString;
+    }
+
+    public static CodigoToken BuscarTokenPorString(String tokenString) {
+        for (CodigoToken token : CodigoToken.values()) {
+            if (token.getTokenString().equals(tokenString)) {
+                return token;
+            }
+        }
+        return null;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,67 @@
 package org.example;
 
-import regerx.Regex;
+import java.util.ArrayList;
+import enuns.*;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        String frase = "void main{inicio;fim}";
+        ArrayList<Integer> tokens = new ArrayList<>();
+        int i = 0;
 
-        System.out.println();
+        while (i < frase.length()) {
+            StringBuilder tokenBuilder = new StringBuilder();
+            char currentChar = frase.charAt(i);
 
+            if (Character.isWhitespace(currentChar)) {
+                i++;
+                continue;
+            }
+
+            if (Character.isLetter(currentChar) || currentChar == '_') {
+                int j = i + 1;
+                while (j <= frase.length()) {
+                    String subString = frase.substring(i, j);
+                    CodigoToken token = CodigoToken.BuscarTokenPorString(subString);
+                    if (token != null) {
+                        tokens.add(token.getCodigo());
+                        i = j;
+                        break;
+                    }
+                    j++;
+                }
+
+                if (j == i + 1) {
+                    while (i < frase.length() && (Character.isLetterOrDigit(frase.charAt(i)) || frase.charAt(i) == '_')) {
+                        tokenBuilder.append(frase.charAt(i));
+                        i++;
+                    }
+                    
+                    String tokenString = tokenBuilder.toString();
+                    CodigoToken token = CodigoToken.BuscarTokenPorString(tokenString);
+                    if (token != null) {
+                        tokens.add(token.getCodigo());
+                    }
+                }
+            } else {
+                // Verifica subsequências de tokens
+                int j = i + 1;
+                while (j <= frase.length()) {
+                    String subString = frase.substring(i, j);
+                    CodigoToken token = CodigoToken.BuscarTokenPorString(subString);
+                    if (token != null) {
+                        tokens.add(token.getCodigo());
+                        i = j;
+                        break;
+                    }
+                    j++;
+                }
+                // Se nenhum token correspondente for encontrado, avança para o próximo caractere
+                if (j == i + 1) {
+                    i++;
+                }
+            }
+        }
+        System.out.println("Números dos tokens encontrados: " + tokens);
     }
 }
